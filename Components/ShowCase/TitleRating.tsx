f9b5ad612d08634732eb3e429c6340ef770ec7a7
fun time <mrjgaming4@gmail.com>
2024-03-12 13:34:50 +0400

    sixth commit

--- Components/ShowCase/TitleRating.tsx
+++ Components/ShowCase/TitleRating.tsx
@@ -4,6 +4,7 @@ import { Rating } from '@mui/material';
 export function TitleRating({
     vote_average, title, rating, air_date, extrastyle
 }: { vote_average?: number, title: string, rating?: boolean, air_date?: string, extrastyle?: string }) {
+
     return <div className={`flex justify-start items-center gap-2  text-2xl
                 sm:text-3xl
                 md:text-3xl
@@ -19,7 +20,7 @@ export function TitleRating({
         {
             /* rating */
         }
-        {rating ? <Rating size={'medium'} readOnly defaultValue={2.5} value={vote_average / 2} /> : null}
+        {rating && vote_average ? <Rating size={'medium'} readOnly defaultValue={2.5} value={vote_average / 2} /> : null}
         {air_date ? <p className="text-slate-400 font-semibold text-base">{air_date}</p> : null}
     </div>;
 }
