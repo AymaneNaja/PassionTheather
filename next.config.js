ffddace6642c7081c3909f4b86a0c73ace03f301
fun time <mrjgaming4@gmail.com>
2024-03-17 14:37:47 +0400

    elevnth commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    reactStrictMode: true, images: { loader: "default", minimumCacheTTL: 60, domains: [ "image.tmdb.org" ], }, 
 }
 
 module.exports = nextConfig
